aa6d2d6ab57bc6f680d79a3ca9484bbb866c927e
Nguyen-van-toan <toanvippk115@gmail.com>
2023-08-01 13:17:33 +0700

    tai lai file

--- src/presentation/ManagementAppInfor.java
+++ src/presentation/ManagementAppInfor.java
@@ -13,6 +13,78 @@ public class ManagementAppInfor {
     private String quota;
     private String nationality;
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getCustomerObject() {
+        return customerObject;
+    }
+
+    public void setCustomerObject(String customerObject) {
+        this.customerObject = customerObject;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(String unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getQuota() {
+        return quota;
+    }
+
+    public void setQuota(String quota) {
+        this.quota = quota;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
     public ManagementAppInfor(String fullName, String customerId, String customerType, String customerObject,
                            String invoiceDate, String quantity, String unitPrice, String quota, String nationality) {
         this.fullName = fullName;
