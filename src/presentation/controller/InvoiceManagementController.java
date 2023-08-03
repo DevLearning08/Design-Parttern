1c6aab89bdf1e2f179de28b601ed08086d044281
ChiAnhNguyen <nguyenchianh2342003@gmail.com>
2023-08-03 17:01:08 +0700

    NUN

--- src/presentation/controller/InvoiceManagementController.java
+++ src/presentation/controller/InvoiceManagementController.java
@@ -1,13 +1,12 @@
 package presentation.controller;
 
 import presentation.ManagementApp;
-import presentation.ManagementAppInfor;
-import presentation.ManagementAppInput;
+
 import java.awt.event.ActionEvent;
 public class InvoiceManagementController {
     private ManagementApp managementAppRemote;
-    private ManagementAppInfor managementAppInforRemote;
-    private ManagementAppInput managementAppInputRemote;
+    
+    
 
 
     public InvoiceManagementController() {
@@ -24,27 +23,7 @@ public class InvoiceManagementController {
         this.managementAppRemote = managementAppRemote;
     }
 
-
-    public ManagementAppInfor getManagementAppInforRemote() {
-        return managementAppInforRemote;
-    }
-
-
-    public void setManagementAppInforRemote(ManagementAppInfor managementAppInforRemote) {
-        this.managementAppInforRemote = managementAppInforRemote;
-    }
-
-
-    public ManagementAppInput getManagementAppInputRemote() {
-        return managementAppInputRemote;
-    }
-
-
-    public void setManagementAppInputRemote(ManagementAppInput managementAppInputRemote) {
-        this.managementAppInputRemote = managementAppInputRemote;
-    }
-    
-
+   
     public void actionPerformed(ActionEvent e) {
         
     }
