05ace204de903c02d818d1ce368ed6454be4de97
Tuthan1103 <thienngon112003@gmail.com>
2023-08-08 10:53:50 +0700

    push

--- src/domain/HoaDon_Sua.java
+++ src/domain/HoaDon_Sua.java
@@ -6,9 +6,11 @@ import pesistence.HoaDonDAO;
 import pesistence.HoaDonDAOImpl;
 
 public class HoaDon_Sua extends HoadonService{
+    HoaDon hoaDon;
     public void action(HoaDon hoaDon,int maKH,int maHD,String hotenKH, String customerType){
         HoaDonDAO hoaDon_sua = new HoaDonDAOImpl();
         hoaDon_sua.suaHoaDon(hoaDon);
+        hoaDon.thongBao();
     }
     
 }
